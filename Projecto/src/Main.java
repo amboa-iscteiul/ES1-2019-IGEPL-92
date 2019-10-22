b3887404f38b4c0d71206530a1f289fbbf482466
pjnoa1-iscteiul <pjnoa1@iscte-iul.pt>
2019-10-22 21:36:56 +0100

    develop/83008- limpei código

--- Projecto/src/Main.java
+++ Projecto/src/Main.java
@@ -1,15 +1,8 @@
 
 public class Main {
-	
-	int a;
 
 	public static void main(String[] args) {
 		System.out.println("grupo 92");
 	}
-	
-	public void master(){
-		System.out.println("master");
-	}
-	
 
 }
