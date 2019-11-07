df38de7d80cd094ca5ebd96d62785fdb6a47a003
amboa-iscteiul <amboa@iscte-iul.pt>
2019-11-07 20:44:41 +0000

    António Oliveira- limpei código

--- Projecto/src/Main.java
+++ Projecto/src/Main.java
@@ -1,15 +1,8 @@
 
 public class Main {
 	
-	public int attribute = 5;
-	public int a;
-
 	public static void main(String[] args) {
 		System.out.println("grupo 92");
 	}
 	
-	public boolean success(boolean b) {
-		return b;
-	}
-
 }
