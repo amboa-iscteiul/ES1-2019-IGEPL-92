424b86a4be70e63254ca6705680e6faa99ae0bb7
amboa-iscteiul <amboa@iscte-iul.pt>
2019-11-09 20:26:01 +0000

    Classe Main.java actualizada

--- Projecto/src/Main.java
+++ Projecto/src/Main.java
@@ -1,15 +1,8 @@
 
 public class Main {
-	
-//	Elementos que já experimentaram demo Git
-	
-	
+
 	public static void main(String[] args) {
 		System.out.println("grupo 92");
 	}
-	
-	private void teste_bea_guia(){
-		System.out.println("Bea Guia no Git");
-	}
-	
+
 }
