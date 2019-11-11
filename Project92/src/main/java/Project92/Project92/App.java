1966de24795b45f690c3b3186cb2dd6a53323eef
baknu-iscteiul <baknu@iscte-iul.pt>
2019-11-11 18:55:04 +0000

    inseri um atributo

--- Project92/src/main/java/Project92/Project92/App.java
+++ Project92/src/main/java/Project92/Project92/App.java
@@ -4,8 +4,10 @@ package Project92.Project92;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+	
+	private int a;
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
