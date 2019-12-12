b07b1336661cd8034b1004f3d5e972ed2f5b3cae
pjnoa1-iscteiul <pjnoa1@iscte-iul.pt>
2019-12-12 20:11:37 +0000

    Paulo Jin- correcao de um erro

--- Project92/src/main/java/Project92/Project92/Table.java
+++ Project92/src/main/java/Project92/Project92/Table.java
@@ -11,16 +11,25 @@ import javax.swing.ViewportLayout;
 
 public class Table {
 	
-	
-	
+	public static Avaliacao_Ferramentas Avaliacao_Ferramentas = new Avaliacao_Ferramentas();
+
+	String pmddci =(""+Avaliacao_Ferramentas.dci("PMD"));
+	String pmddii =(""+Avaliacao_Ferramentas.dii("PMD"));
+	String pmdadci =(""+Avaliacao_Ferramentas.adci("PMD"));
+	String pmdadii =(""+Avaliacao_Ferramentas.adii("PMD"));
+	String iplasmadci =(""+Avaliacao_Ferramentas.dci("iPlasma"));
+	String iplasmadii =(""+Avaliacao_Ferramentas.dii("iPlasma"));
+	String iplasmaadci =(""+Avaliacao_Ferramentas.adci("iPlasma"));
+	String iplasmaadii =(""+Avaliacao_Ferramentas.adii("iPlasma"));
+
+
 	public void criarJanela() {
 	JFrame aux= new JFrame();
 	Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
 	aux.setLocation(dimension.width / 2 - (220 / 2), dimension.height / 2 - (250 / 2));
 	aux.setLayout(new BorderLayout());
-//	JPanel p = new JPanel(new BorderLayout());
-	String data[][] = {{"PMD","140","18","261","0"},
-						{"iPlasma","140","0","279","0"}};
+	String data[][] = {{"PMD",pmddci,pmddii,pmdadci,pmdadii},
+						{"iPlasma",iplasmadci,iplasmadii,iplasmaadci,iplasmadii}};
 	String column[] = {"Description","DCI","DII","ADCI","ADII"};
 	JTable tab= new JTable(data,column);
 	JScrollPane b= new  JScrollPane(tab);
