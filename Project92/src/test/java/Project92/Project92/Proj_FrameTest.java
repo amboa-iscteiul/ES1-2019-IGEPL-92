7ce84a8870331cf296f4eb14ee01f52b4e8a94ec
fmmdu-iscteiul <fmmdu@iscte-iul.pt>
2019-12-13 17:01:17 +0000

    atualização frame test

--- Project92/src/test/java/Project92/Project92/Proj_FrameTest.java
+++ Project92/src/test/java/Project92/Project92/Proj_FrameTest.java
@@ -2,7 +2,18 @@ package Project92.Project92;
 
 import junit.framework.TestCase;
 
+import javax.swing.JFrame;
+
+import org.junit.*;
+
 public class Proj_FrameTest extends TestCase {
+	
+	public JFrame frame;
+	
+	@Before
+	public void init() {
+		frame = new JFrame("teste");
+	}
 
 	public void testProj_Frame() {
 		fail("Not yet implemented");
